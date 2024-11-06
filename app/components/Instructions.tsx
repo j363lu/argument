cc8db36c52bcd186a26e4670cbf77d219fb13ac2
j363lu <j363lu@uwaterloo.ca>
2024-11-06 21:19:05 +0800

    update instruction

--- app/components/Instructions.tsx
+++ app/components/Instructions.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
+import { selectTopic } from "@/lib/typeSlice";
 // import NavigationButton from "./NavigationButton";
 
 function Instructions() {
+  const topic = useAppSelector(selectTopic);
+
+  const kidneyMarkets = "whether it should be legal for people to buy and sell kidneys";
+  const freeTrade = "whether free trade should be allowed";
+
   return (
     <div className="flex flex-col space-y-4 p-7 sm:p-10">
       <h3 className='text-3xl font-semibold text-black'>Instructions</h3>
       <p>
-        Type a brief sentence that says if you are for, against, or have mixed feelings on whether it should be legal for people to buy and sell kidneys. You will then be presented with an argument and be given three opportunities to reply with the interface.
+        Type a brief sentence that says if you are for, against, or have mixed feelings on {topic == "freeTrade" ? freeTrade : kidneyMarkets}. You will then be presented with an argument and be given three opportunities to reply with the interface.
       </p>
 
       {/* <NavigationButton /> */}
